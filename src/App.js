1749542820ceeb96bca5f3e52637d259dae1c14f
Kayra Kavlak <kayranium10@gmail.com>
2023-02-05 23:54:10 +0300

    v1.0, First release

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
   
   const[newThing, setNewThing] = useState("");
   const[things, setThings] = useState([]);
+  
+  useEffect(()=>{
+    if(localStorage.getItem('stuff')){
+      const stored = JSON.parse(localStorage.getItem('stuff'));
+      setThings(stored);
+    }
+  },[]);
 
   function addAThing(){
     
@@ -13,35 +20,40 @@ function App() {
       id:things.length + 1
     };
 
-    setThings(oldList => [...oldList, thing]);
-    setNewThing("");
-    //console.log(thing.id)
+    setThings([...things,thing]);
+    localStorage.setItem('stuff',JSON.stringify([...things,thing]))
+    setNewThing('');
   }
   
   function deleteThing(id){
     const newArray = things.filter(thing => thing.id !== id);
     setThings(newArray);
+    localStorage.setItem('stuff',JSON.stringify(newArray));
   }
 
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <p>by kayra</p>
+      <h1>To-Do List</h1>
+      <p>by Kayra</p>
       
       <input 
           type="text" 
           placeholder='Type a thing to do...'
           onChange={e => setNewThing(e.target.value)} />
       
-      <button onClick={() => addAThing()}>Add to list</button>
+      <button id='adder' onClick={() => addAThing()}>Add</button>
 
+      <table>
       <ul>
         {things.map(thing =>{
           return(
-            <li key={thing.id}>{thing.value} <button onClick={() => deleteThing(thing.id)}>X</button> </li>
-          )
+            <li key={thing.id}>
+              {thing.value}
+              <button onClick={() => deleteThing(thing.id)}>X</button>
+            </li> )
         })}
       </ul>
+      </table>
 
     </div>
   );
